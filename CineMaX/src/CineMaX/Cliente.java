92558c34eb1fe9e10ef8f934e4e4d8f18d93fa30
Nithay <npgomezsilva@studenti.uninsubria.it>
2026-05-16 19:51:47 +0200

    costruttore ciente.java

--- CineMaX/src/CineMaX/Cliente.java
+++ CineMaX/src/CineMaX/Cliente.java
@@ -2,4 +2,37 @@ package CineMaX;
 
 public class Cliente extends Guest {
 
+    //parametri //
+
+    // private String IDCliente;
+    //costruttore//
+
+    public Cliente (String nome, String cognome, String password, int id ){
+        super(nome, cognome, "C",password, id);
+    }
+
+
+      //metodi//
+    // public String getIDCliente(){
+    //     return this.IDCliente;
+    // }
+    //il set no si implementa perche non si deve mai modificare l'ID dalla creazione//
+    
+      //-inserire una prenotazione//
+
+      //- cerca proiezione (implementato da guest)//
+      // CercaFilm()
+
+      //- visulazza proiezione (implementato da guest)//
+      //DettagliFilm()
+
+      //-visualizzare le propie prenotazioni//
+      //crea prenotazione//
+      //modifica prenotazione//
+      //elimina prenotazione//
+
+      //-modificare e cancellare le proprie prenotazioni//
+      //-logout//
+
+
 }
