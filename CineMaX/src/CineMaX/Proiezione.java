44208202112b70d3e4a4e688874c2eebb5445543
PasqualeDiTuccio766922 <pdituccio@studenti.uninsubria.it>
2026-08-28 13:21:04 +0200

    Update Proiezione.java
    rimosso local date

--- CineMaX/src/CineMaX/Proiezione.java
+++ CineMaX/src/CineMaX/Proiezione.java
@@ -5,7 +5,6 @@ import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
